0d1223f5a15d25df8851c2cc1d6ad7361a422cad
Steven Barkley <steven.barkley@outlook.com>
2022-06-01 04:36:49 -0400

    Blah

--- Animal_Fun_Facts/Development/animal-fun-facts/src/App.js
+++ Animal_Fun_Facts/Development/animal-fun-facts/src/App.js
@@ -4,11 +4,25 @@ import { navBar } from './animals';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+const background = <img class="background" alt="ocean" src="/images/ocean.jpg" />;
+
+const title = "Click an animal for a fun fact";
+
+const animalFacts = (
+  <div>
+    <h1>{ title }</h1>
+    { background }
+  </div>
+);
+
+title === "" ? "Click an animal for a fun fact" : title
+
+const images = [];
+
 function App() {
   return (
     <div className="App">
-      <p> Does the { navBar } variable work in JSX?</p>
-
+      <p>Test { title }</p>
     </div>
   );
 }
